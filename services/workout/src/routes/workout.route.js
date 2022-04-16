395048dcaba9cfa5249f4706668e728d9b68a0d2
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2022-04-16 20:36:38 +0200

    test

--- services/workout/src/routes/workout.route.js
+++ services/workout/src/routes/workout.route.js
@@ -1,22 +1,22 @@
-const workout = require("../controllers/workout.controller");
-const activity = require("../controllers/activity.controller");
+const workout = require('../controllers/workout.controller')
+const activity = require('../controllers/activity.controller')
 
 module.exports = (app) => {
-  const router = require("express").Router();
+  const router = require('express').Router()
 
-  router.post("/workouts", workout.create);
-  router.post("/exercices", workout.createExercice);
-  router.post("/workouts/:id/activity", activity.createActivity);
+  router.post('/workouts', workout.create)
+  router.post('/exercices', workout.createExercice)
+  router.post('/workouts/:id/activity', activity.createActivity)
 
-  router.get("/workouts", workout.findAll);
-  router.get("/exercices", workout.findAllExercices);
-  router.get("/activities", activity.findAll);
+  router.get('/workouts', workout.findAll)
+  router.get('/exercices', workout.findAllExercices)
+  router.get('/activities', activity.findAll)
   //   router.get("/workouts/categories", reward.findAllCategories);
-
+  // test comment
   //   router.patch("/workouts/:id", reward.update);
 
   //   router.delete("/workouts/all", reward.deleteAll);
-  router.delete("/workouts/:id", workout.deleteById);
+  router.delete('/workouts/:id', workout.deleteById)
 
-  app.use("/api", router);
-};
+  app.use('/api', router)
+}
