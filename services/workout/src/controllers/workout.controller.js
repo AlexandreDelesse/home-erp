02d102c61b7555de9acfcb37bf1f6ab2457b210e
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2022-04-16 22:32:03 +0200

    HOM-5 CRUD workout

--- services/workout/src/controllers/workout.controller.js
+++ services/workout/src/controllers/workout.controller.js
@@ -1,61 +1,94 @@
-const db = require("../models");
+const db = require('../models')
 
-const Workout = db.workout;
-const Activity = db.activity;
-const Exercice = db.exercice;
-const Serie = db.serie;
+const Workout = db.workout
+const Activity = db.activity
+const Exercice = db.exercice
+const Serie = db.serie
 
 exports.create = async (req, res) => {
-  newWorkout = req.body;
+  newWorkout = req.body
 
   try {
     const result = await Workout.create(newWorkout, {
       include: [{ model: Activity, include: [Serie] }],
-    });
+    })
 
-    res.status(201).send(result);
+    res.status(201).send(result)
   } catch (err) {
-    console.log(err);
-    res.status(500).send({ message: "Failed to create Workout", err });
+    console.log(err)
+    res.status(500).send({ message: 'Failed to create Workout', err })
   }
-};
+}
 
 exports.createExercice = async (req, res) => {
-  newExercice = req.body;
+  newExercice = req.body
 
   try {
-    const result = await Exercice.create(newExercice);
-    res.status(201).send(result);
+    const result = await Exercice.create(newExercice)
+    res.status(201).send(result)
   } catch (err) {
-    res.status(500).send({ message: "Failed to create Exercice", err });
+    res.status(500).send({ message: 'Failed to create Exercice', err })
   }
-};
+}
+
+exports.updateById = async (req, res) => {
+  newWorkout = req.body
+  const { id } = req.params
+
+  try {
+    const result = await Workout.update(newWorkout, { where: { id } })
+    res.status(200).send(result)
+  } catch (err) {
+    res.status(500).send({ message: 'Failed to update Workout', err })
+  }
+}
 
 exports.findAll = async (req, res) => {
-  console.log("get workouts");
+  console.log('get workouts')
   try {
     const result = await Workout.findAll({
       include: [
         { model: Activity, include: [{ model: Exercice }, { model: Serie }] },
       ],
-    });
-    res.status(200).send(result);
+    })
+    res.status(200).send(result)
   } catch (err) {
-    console.log(err);
-    res.status(500).send({ message: "Failed to get all Workouts", err });
+    console.log(err)
+    res.status(500).send({ message: 'Failed to get all Workouts', err })
   }
-};
+}
+
+exports.findById = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const result = await Workout.findOne({
+      where: { id },
+      include: [
+        { model: Activity, include: [{ model: Exercice }, { model: Serie }] },
+      ],
+    })
+    if (!result) {
+      res.status(404).send({ message: 'Not found' })
+      return
+    }
+    res.status(200).send(result)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({ message: 'Failed to get all Workouts', err })
+  }
+}
 
 exports.findAllExercices = async (req, res) => {
-  console.log("get workouts");
+  console.log('get workouts')
   try {
-    const result = await Exercice.findAll();
-    res.status(200).send(result);
+    const result = await Exercice.findAll()
+    res.status(200).send(result)
   } catch (err) {
-    console.log(err);
-    res.status(500).send({ message: "Failed to get all Exercices", err });
+    console.log(err)
+    res.status(500).send({ message: 'Failed to get all Exercices', err })
   }
-};
+}
 
 // exports.findAllCategories = async (req, res) => {
 //   try {
@@ -83,20 +116,20 @@ exports.findAllExercices = async (req, res) => {
 // };
 
 exports.deleteById = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   try {
     await Workout.destroy({
       where: { id },
       truncate: false,
-    });
-    res.sendStatus(200);
+    })
+    res.sendStatus(200)
   } catch (err) {
     res.status(500).send({
-      message: "Failed to delete Workout with id : " + id,
+      message: 'Failed to delete Workout with id : ' + id,
       err,
-    });
+    })
   }
-};
+}
 
 // exports.deleteAll = async (req, res) => {
 //   try {
--- services/workout/src/routes/workout.route.js
+++ services/workout/src/routes/workout.route.js
@@ -4,19 +4,23 @@ const activity = require('../controllers/activity.controller')
 module.exports = (app) => {
   const router = require('express').Router()
 
+  // Workout
   router.post('/workouts', workout.create)
-  router.post('/exercices', workout.createExercice)
-  router.post('/workouts/:id/activity', activity.createActivity)
-
   router.get('/workouts', workout.findAll)
+  router.get('/workouts/:id', workout.findById)
+  router.delete('/workouts/:id', workout.deleteById)
+  router.patch('/workouts/:id', workout.updateById)
+
+  // Exercice
   router.get('/exercices', workout.findAllExercices)
   router.get('/activities', activity.findAll)
+  router.post('/exercices', workout.createExercice)
+  router.post('/workouts/:id/activity', activity.createActivity)
   //   router.get("/workouts/categories", reward.findAllCategories);
-  
+
   //   router.patch("/workouts/:id", reward.update);
 
   //   router.delete("/workouts/all", reward.deleteAll);
-  router.delete('/workouts/:id', workout.deleteById)
   router.delete('/activities/:id', activity.deleteById)
 
   app.use('/api', router)
